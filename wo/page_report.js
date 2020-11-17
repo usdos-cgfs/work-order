6cef9a4557bfddca009ef5a336f33ad5f65f1dd6
Peter F. Backlund <backlundpf@state.gov>
2020-11-17 20:49:56 +0000

    switching back to charleston

--- wo/page_report.js
+++ wo/page_report.js
@@ -1,1 +1,15 @@
-var str = Let's rebuild from the ground up
+var Workorder = window.Workorder || {};
+Workorder.Report = Workorder.Report || {};
+
+function initReportPage() {
+  Workorder.Report.Report = new Workorder.Report.NewReportPage();
+}
+
+Workorder.Report.NewReportPage = function () {
+  function reportViewmodel() {
+    let self = this;
+    self.view = ko.observable();
+    self.viewOpts = ko.observableArray(["Closed", "Open"]);
+  }
+  vm._report = new reportViewmodel();
+};
