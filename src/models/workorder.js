67bd7fd5d3ed78d7f774e688f98cccdb58a7c6cd
Peter F. Backlund <backlundpf@state.gov>
2023-04-04 14:05:33 -0400

    initialized project

---
+++ src/models/workorder.js
@@ -0,0 +1,5 @@
+import { SPList } from "../common/sal.js";
+
+export default function WorkOrder() {
+  const list = new SPList({});
+}
--- src/page_wo_db.js
+++ src/page_wo_db.js
@@ -1,6 +1,8 @@
+import WorkOrder from "./models/workorder.js";
+
 export default {
   data() {
-    return { count: 0 };
+    return { count: 0, workOrder: new WorkOrder() };
   },
   template: `<div>count is {{ count }}</div>`,
 };
