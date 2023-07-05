8ec7c0fcdd18700fc9cf0de3e0b4607d54c92286
Peter F. Backlund <backlundpf@state.gov>
2023-07-05 12:47:14 -0400

    finalized report

--- src/report.js
+++ src/report.js
@@ -61,13 +61,15 @@ class ServiceTypeSummary {
 
   toggleViewLate = () => this.ViewLate(!this.ViewLate());
 
-  AverageCompletionTime = ko.pureComputed(() => {
-    if (!this.requests.length) return;
-
-    const totalDays = this.requests.reduce(
+  totalCompletionTime = () =>
+    this.requests.reduce(
       (accumulator, request) => accumulator + request.Reporting.OpenDays(),
       0
     );
+  AverageCompletionTime = ko.pureComputed(() => {
+    if (!this.requests.length) return;
+
+    const totalDays = this.totalCompletionTime();
     return Math.ceil(totalDays / this.requests.length);
   });
 }
@@ -101,46 +103,12 @@ class Report {
     holidayStore,
   };
 
-  async init() {
-    configLists: {
-      var pipelinesPromise = this._context.ConfigPipelines.ToList().then(
-        this.Config.pipelineStageStore
-      );
-
-      var requestOrgsPromise = this._context.ConfigRequestOrgs.ToList().then(
-        (arr) => this.Config.requestOrgStore(arr.sort(sortByTitle))
-      );
-
-      var serviceTypePromise = this._context.ConfigServiceTypes.ToList().then(
-        (arr) => this.Config.serviceTypeStore(arr.sort(sortByTitle))
-      );
-
-      const holidaysPromise = this._context.ConfigHolidays.ToList().then(
-        this.Config.holidayStore
-      );
-
-      const configResults = await Promise.all([
-        requestOrgsPromise,
-        pipelinesPromise,
-        serviceTypePromise,
-        holidaysPromise,
-      ]);
-    }
-
-    user: {
-      this._currentUser(await User.Create());
-    }
-
-    const requests = await this._context.Requests.ToList();
-    this.AllRequests(requests);
-  }
-
-  requestStates = requestStates;
-  AllRequests = ko.observableArray();
+  allRequests = [];
+  allAssignments = [];
 
   filters = {
     org: {
-      Value: ko.observable("temp"),
+      Value: ko.observable(),
       Options: ko.pureComputed(
         () => this._currentUser()?.ActionOffices() ?? []
       ),
@@ -148,11 +116,25 @@ class Report {
     status: {
       Value: ko.observable(),
       Options: ko.pureComputed(() => [
-        requestStates.open,
         requestStates.fulfilled,
+        requestStates.open,
       ]),
     },
-    user: new PeopleField({ displayName: "Action Office" }),
+    user: {
+      Value: ko.observable(),
+      Options: ko.pureComputed(() => {
+        const assignees = this.allAssignments.map(
+          (assignment) => assignment.Assignee
+        );
+
+        return assignees
+          .filter(
+            (assignee, index, self) =>
+              assignee && index === self.findIndex((t) => t?.ID === assignee.ID)
+          )
+          .sort(sortByTitle);
+      }),
+    },
     allDates: new CheckboxField({
       displayName: "All Dates",
       Visible: ko.pureComputed(
@@ -213,12 +195,26 @@ class Report {
     // );
 
     // Filter by status
-    const filteredRequests = this.AllRequests().filter(
+    let filteredRequests = this.allRequests.filter(
       (request) => request.State.Status() == this.filters.status.Value()
     );
 
-    // Filter by AO
-
+    // Filter by AO/User
+    if (this.filters.user.Value()) {
+      const userId = this.filters.user.Value().ID;
+      //filteredRequests.filter();
+      const requestIds = new Set();
+      // 1 filter all assignments assigned to user
+      // 2 add to unique set of requests ids
+      this.allAssignments
+        .filter((assignment) => assignment.Assignee?.ID === userId)
+        .map((assignment) => requestIds.add(assignment.Request.ID));
+      // 3 filter requests by assignment request ids
+      filteredRequests = filteredRequests.filter((request) =>
+        requestIds.has(request.ID)
+      );
+    }
+    // Guard condition
     if (
       this.filters.status.Value() == requestStates.open ||
       this.filters.allDates.Value()
@@ -236,6 +232,14 @@ class Report {
     );
   });
 
+  FilteredAssignments = ko.pureComputed(() => {
+    return this.FilteredRequests().flatMap((request) => {
+      return this.allAssignments.filter(
+        (assignment) => assignment.Request.ID == request.ID
+      );
+    });
+  });
+
   ServiceTypeReports = ko.pureComputed(() => {
     console.log("Generate Reports Triggered");
     const reports = [];
@@ -246,10 +250,82 @@ class Report {
         (request) => service.ID == request.ServiceType.Def()?.ID
       );
 
+      const requestIds = requests.map((request) => request.ID);
+
       reports.push(new ServiceTypeSummary({ service, requests, thresholds }));
     });
     return reports;
   });
+
+  SummaryRow = ko.pureComputed(() => {
+    const summary = {
+      totalOpen: 0,
+      totalMeetingStandard: 0,
+      totalNotMeetingStandard: 0,
+      percentMeetingStandard: 0,
+      totalCompletionTime: 0,
+      averageCompletionTime: 0,
+    };
+
+    this.ServiceTypeReports().map((service) => {
+      summary.totalOpen += service.requests.length;
+      summary.totalMeetingStandard += service.MeetingStandardCount();
+      summary.totalNotMeetingStandard += service.NotMeetingStandard().length;
+      summary.totalCompletionTime += service.totalCompletionTime();
+    });
+
+    summary.percentMeetingStandard = Math.round(
+      (summary.totalMeetingStandard / summary.totalOpen) * 100
+    );
+
+    summary.averageCompletionTime = Math.ceil(
+      summary.totalCompletionTime / summary.totalOpen
+    );
+
+    return summary;
+  });
+
+  async init() {
+    configLists: {
+      var pipelinesPromise = this._context.ConfigPipelines.ToList().then(
+        this.Config.pipelineStageStore
+      );
+
+      var requestOrgsPromise = this._context.ConfigRequestOrgs.ToList().then(
+        (arr) => this.Config.requestOrgStore(arr.sort(sortByTitle))
+      );
+
+      var serviceTypePromise = this._context.ConfigServiceTypes.ToList().then(
+        (arr) => this.Config.serviceTypeStore(arr.sort(sortByTitle))
+      );
+
+      const holidaysPromise = this._context.ConfigHolidays.ToList().then(
+        this.Config.holidayStore
+      );
+
+      const configResults = await Promise.all([
+        requestOrgsPromise,
+        pipelinesPromise,
+        serviceTypePromise,
+        holidaysPromise,
+      ]);
+    }
+
+    user: {
+      this._currentUser(await User.Create());
+    }
+
+    records: {
+      await Promise.all([
+        this._context.Requests.ToList().then(
+          (requests) => (this.allRequests = requests)
+        ),
+        this._context.Assignments.ToList().then(
+          (assignments) => (this.allAssignments = assignments)
+        ),
+      ]);
+    }
+  }
 }
 
 async function createReport() {
