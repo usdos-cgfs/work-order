546b8695b22c4cbf1e6b7fe7b5e170c3ed8add15
Peter F. Backlund <backlundpf@state.gov>
2023-12-13 09:28:09 -0500

    By Service Type restricted to service types user can initiate

--- src/components/RequestsByServiceType/RequestsByServiceTypeModule.js
+++ src/components/RequestsByServiceType/RequestsByServiceTypeModule.js
@@ -1,11 +1,16 @@
 import { serviceTypeStore } from "../../entities/ServiceType.js";
+import { currentUser } from "../../infrastructure/Authorization.js";
 
 export default class RequestsByServiceTypeModule {
   constructor(params) {
     this.key = params.key;
   }
 
-  ServiceTypes = serviceTypeStore;
+  ServiceTypes = ko.pureComputed(() =>
+    serviceTypeStore().filter((serviceType) =>
+      serviceType.userCanInitiate(currentUser())
+    )
+  );
 
   SelectedService = ko.observable();
 }
