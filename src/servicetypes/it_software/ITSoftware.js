34f342f90212a09afc7e554752c5579816f13162
backlundpf <46462897+peterfbacklund@users.noreply.github.com>
2024-06-28 14:15:20 -0400

    add IT software to procurement action

--- src/servicetypes/it_software/ITSoftware.js
+++ src/servicetypes/it_software/ITSoftware.js
@@ -2,84 +2,18 @@ import { TextField, SelectField } from "../../fields/index.js";
 
 import { defaultComponents } from "../../primitives/ConstrainedEntity.js";
 
-import { getAppContext } from "../../infrastructure/ApplicationDbContext.js";
-import { currentUser } from "../../infrastructure/Authorization.js";
-
-import { requestOrgStore } from "../../entities/RequestOrg.js";
-import { serviceTypeStore } from "../../entities/ServiceType.js";
-import { RequestEntity } from "../../entities/Request.js";
 import { BaseServiceDetail } from "../BaseServiceDetail.js";
 import { ConstrainedEntityViewModule } from "../../components/index.js";
 import { itSoftwareViewTemplate } from "./views/View.js";
 import { registerComponentFromConstructor } from "../../infrastructure/RegisterComponents.js";
+import { CreateProcurementAction } from "./components/CreateProcurementAction.js";
 
-const components = {
-  ...defaultComponents,
-  view: "svc-it_software-view",
-};
-
-class ITSoftwareViewModule extends ConstrainedEntityViewModule {
-  constructor(params) {
-    super(params);
-  }
-
-  static name = components.view;
-  static template = itSoftwareViewTemplate;
-}
-
-registerComponentFromConstructor(ITSoftwareViewModule);
+registerComponentFromConstructor(CreateProcurementAction);
 
 export class ITSoftware extends BaseServiceDetail {
   constructor(params) {
     super(params);
   }
-
-  ShowCreateProcurementButton = ko.pureComputed(() => {
-    const itOrg = requestOrgStore().find((org) => org.Title == "CGFS/EX/IT");
-    return itOrg && currentUser()?.isInRequestOrg(itOrg);
-  });
-
-  createProcurement = async () => {
-    // 1. Generate the description of the new request
-    let procurementDescription = "<ul>";
-    Object.values(this.FieldMap).forEach((field) => {
-      if (field) {
-        procurementDescription += `<li>${
-          field.displayName
-        }: ${field.toString()}</li>`;
-      }
-    });
-    procurementDescription += "</ul>";
-
-    // 2. Create a new request of the procurement
-    const procurementServiceTypeDef = serviceTypeStore().find(
-      (service) => service.UID == "procurement"
-    );
-
-    const newRequest = new RequestEntity({
-      ServiceType: procurementServiceTypeDef,
-    });
-
-    // 3. Populate fields
-    newRequest.FieldMap.RequestDescription.set(procurementDescription);
-    newRequest.FieldMap.RequestingOffice.set(
-      this.Request.FieldMap.RequestingOffice.get()
-    );
-
-    // 4. Copy files
-    const sourcePath = this.Request.getRelativeFolderPath();
-    const targetPath = await newRequest.Attachments.createFolder();
-
-    const _context = getAppContext();
-    try {
-      await _context.Attachments.CopyFolderContents(sourcePath, targetPath);
-      newRequest.Attachments.refresh();
-    } catch (e) {
-      console.error("Error copying files: ", e);
-    }
-    // 5. View Request
-    window.WorkOrder.App.NewRequest({ request: newRequest });
-  };
 
   CostThreshold = ko.pureComputed(
     () => parseInt(this.FieldMap.Cost.Value()) > 500
@@ -125,7 +59,6 @@ export class ITSoftware extends BaseServiceDetail {
     }),
   };
 
-  components = components;
   static Views = {
     All: ["ID", "Title", "Request"],
   };
