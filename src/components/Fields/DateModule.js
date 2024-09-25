b74a82fc1a70288e5c6b2aec756b737214f1e467
backlundpf <46462897+peterfbacklund@users.noreply.github.com>
2024-09-25 09:57:16 -0400

    fix timezone offset date field issues
    move inputbinding logic to component module
    don't need to convert between utc

--- src/components/Fields/DateModule.js
+++ src/components/Fields/DateModule.js
@@ -4,6 +4,11 @@ import {
   registerFieldComponents,
 } from "./BaseFieldModule.js";
 
+export const dateFieldTypes = {
+  date: "date",
+  datetime: "datetime-local",
+};
+
 const editTemplate = html`
   <label class="fw-semibold"
     ><span data-bind="text: displayName"></span
@@ -30,6 +35,35 @@ export class DateModule extends BaseFieldModule {
   constructor(params) {
     super(params);
   }
+
+  toInputDateString = () => this.Value().format("yyyy-MM-dd");
+
+  toInputDateTimeString = () => this.Value().format("yyyy-MM-ddThh:mm");
+
+  inputBinding = ko.pureComputed({
+    read: () => {
+      if (!this.Value()) return null;
+      switch (this.type) {
+        case dateFieldTypes.date:
+          return this.toInputDateString();
+        case dateFieldTypes.datetime:
+          return this.toInputDateTimeString();
+        default:
+          return null;
+      }
+    },
+    write: (val) => {
+      if (!val) return;
+      //writes in format
+      if (this.type == dateFieldTypes.datetime) {
+        this.Value(new Date(val));
+        return;
+      }
+
+      // make sure we're using midnight local time
+      this.Value(new Date(val + "T00:00"));
+    },
+  });
 
   static editTemplate = editTemplate;
 
